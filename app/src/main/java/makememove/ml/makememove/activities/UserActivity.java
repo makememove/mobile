f7c5581cfb0721f46053b2c42ce84095e1e6091a
mabeto <mabeto1996@gmail.com>
2018-10-26 20:45:55 +0200

    GUI Design folytatása

--- app/src/main/java/makememove/ml/makememove/activities/UserActivity.java
+++ app/src/main/java/makememove/ml/makememove/activities/UserActivity.java
@@ -1,10 +1,17 @@
 package makememove.ml.makememove.activities;
 
 import android.content.Intent;
+import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
 
 import makememove.ml.makememove.R;
 import makememove.ml.makememove.datahandler.TokenHandler;
@@ -12,7 +19,11 @@ import makememove.ml.makememove.user.Normal;
 import makememove.ml.makememove.user.User;
 
 public class UserActivity extends AppCompatActivity {
-    private Button bt_logout;
+    private ImageButton bt_menu;
+    private ImageButton bt_notification;
+    private PopupWindow menu;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,17 +45,31 @@ public class UserActivity extends AppCompatActivity {
             e.printStackTrace();
         }
 
-        bt_logout=findViewById(R.id.bt_logout);
-        bt_logout.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                TokenHandler tokenHandler= new TokenHandler();
-                tokenHandler.clear();
 
-                Intent intent = new Intent(UserActivity.this, LoginActivity.class);
-                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-                startActivity(intent);
-            }
-        });
+       bt_menu=findViewById(R.id.bt_menu);
+       bt_menu.setOnClickListener(new View.OnClickListener() {
+           public void onClick(View v) {
+               LayoutInflater inflater = getLayoutInflater();
+               LinearLayout header= findViewById(R.id.header);
+               menu = new PopupWindow(inflater.inflate(R.layout.menu ,null, false),Resources.getSystem().getDisplayMetrics().widthPixels/2,FrameLayout.LayoutParams.MATCH_PARENT, true);
+               menu.showAtLocation(findViewById(R.id.content), Gravity.TOP|Gravity.LEFT , 0,0);
+               Button bt_logout=menu.getContentView().findViewById(R.id.bt_logout);
+               bt_logout.setOnClickListener(new View.OnClickListener() {
+                   public void onClick(View v) {
+                       TokenHandler tokenHandler= new TokenHandler();
+                       tokenHandler.clear();
+
+                       Intent intent = new Intent(UserActivity.this, LoginActivity.class);
+                       intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                       startActivity(intent);
+                   }
+               });
+           }
+       });
+
+
+
+
 
     }
 }
