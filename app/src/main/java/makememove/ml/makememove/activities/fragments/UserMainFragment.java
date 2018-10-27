6236c4bdea674d595fdbc6f1e4aff16b4ef6e956
IncredibleK <aron11111@gmail.com>
2018-10-27 20:14:01 +0200

    Kiválasztott sport kiírása

--- app/src/main/java/makememove/ml/makememove/activities/fragments/UserMainFragment.java
+++ app/src/main/java/makememove/ml/makememove/activities/fragments/UserMainFragment.java
@@ -2,9 +2,11 @@ package makememove.ml.makememove.activities.fragments;
 
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AlertDialog;
+import android.util.Log;
 import android.view.ContextThemeWrapper;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,11 +14,16 @@ import android.view.ViewGroup;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import makememove.ml.makememove.R;
 import makememove.ml.makememove.activities.UserActivity;
+import makememove.ml.makememove.globals.GlobalClass;
 
 public class UserMainFragment extends Fragment {
     private ImageButton bt_addsport;
@@ -25,12 +32,12 @@ public class UserMainFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.usermain_fragment, container, false);
     }
@@ -48,14 +55,18 @@ public class UserMainFragment extends Fragment {
                 public void onClick(View view) {
                     AlertDialog.Builder dialog = new AlertDialog.Builder(new ContextThemeWrapper(view.getContext(), R.style.AlertDialogCustom));
                     dialog.setTitle("Choose a Sport");
-                    dialog.setItems(R.array.sports_array,new DialogInterface.OnClickListener() {
+                    dialog.setItems(R.array.sports_array, new DialogInterface.OnClickListener() {
 
                         @Override
                         public void onClick(DialogInterface dialog, int position) {
+
+                            String[] sports = getResources().getStringArray(R.array.sports_array);
+
                             LayoutInflater inflater = getLayoutInflater();
                             LinearLayout mainLayout = Layout.findViewById(R.id.sportslayout);
-                            View mylayout = inflater.inflate(R.layout.sportcsempe, mainLayout, true);
-
+                            View myLayout = inflater.inflate(R.layout.sportcsempe, mainLayout, true);
+                            TextView sportText = (TextView) myLayout.findViewById(R.id.sporttitle);
+                            sportText.setText(sports[position]);
                         }
 
 
@@ -69,10 +80,10 @@ public class UserMainFragment extends Fragment {
                     });
                     AlertDialog alert = dialog.create();
                     alert.show();
-
                 }
             });
         }
+
     }
 }
 
